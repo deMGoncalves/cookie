aec4dbcfc9cd6b92530cc1a8a0ef1c1e29b3c79c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-02-25 08:56:20 -0300

    feat(src): copiando o pacote cooki do kuba

---
+++ src/setItem.js
@@ -0,0 +1,4 @@
+import * as f from 'f'
+
+export default (key, value = '', descriptor = {}) =>
+  (document.cookie = `${key}=${value};domain=${f.or(descriptor.domain, '')};path=${f.or(descriptor.path, '/')};expires=${f.or(descriptor.expires, '')}`)
