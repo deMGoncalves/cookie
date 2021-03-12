1e2b79075ba53f97ada87599cee405aad08558bd
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-12 14:03:24 -0300

    build(dist): automatic new publish

--- dist/setItem.js
+++ dist/setItem.js
@@ -1,2 +1,8 @@
 import * as f from 'f';
-export default (key, value = '', descriptor = {}) => (document.cookie = `${key}=${value};domain=${f.or(descriptor.domain, '')};path=${f.or(descriptor.path, '/')};expires=${f.or(descriptor.expires, '')}`);
+import settings from "./settings";
+export default function (key, value = '', descriptor = {}) {
+    const domain = f.or(descriptor.domain, settings.domain, '');
+    const expires = f.or(descriptor.expires, settings.expirespath, '');
+    const path = f.or(descriptor.path, settings.path, '/');
+    document.cookie = `${key}=${value};domain=${domain};expires=${expires};path=${path}`;
+}
